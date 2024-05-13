63ecb5ac2589238d0005470dec80425bcd2046d5
Nyxenn <jordib.edu@gmail.com>
2024-05-13 17:21:23 +0200

    Use auth for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,38 @@
-import { headers } from "next/headers";
-import Link from "next/link";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
+
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
-  console.log(images);
 
   return (
+    <div className="flex flex-wrap gap-4">
+      {images.map((img) => (
+        <div key={img.id} className="flex w-48 flex-col">
+          <img src={img.url} />
+          <div>{img.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default async function HomePage() {
+  return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {images.map((img) => (
-          <div key={img.id} className="flex w-48 flex-col">
-            <img src={img.url} />
-            <div>{img.name}</div>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">
+          Please sign in above
+        </div>
+      </SignedOut>
+
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
